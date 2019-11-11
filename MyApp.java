3e6d16b1b784d37d44eb7e379ff83f4643315922
PARKSUNGHYUN1 <smecpat@nate.com>
2019-11-11 12:47:56 +0900

    학번이름

--- MyApp.java
+++ MyApp.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class MyApp here.
  *
- * @author (your name)
+ * @author (2017605038 스피뎅 크릴, 2018315031 박성현)
  * @version (a version number or a date)
  */
 public class MyApp
